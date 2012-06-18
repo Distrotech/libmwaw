b306269a025d4fab256101b989e5a358b0ca6b48
laurent alonso <alonso@saturne.loria.fr>
2012-06-18 10:17:30 +0200

    Add 2 forgotten files...

---
+++ src/lib/MSWStruct.cxx
@@ -0,0 +1,500 @@
+/* -*- Mode: C++; c-default-style: "k&r"; indent-tabs-mode: nil; tab-width: 2; c-basic-offset: 2 -*- */
+/* libmwaw
+ * Copyright (C) 2009, 2011 Alonso Laurent (alonso@loria.fr)
+ * Copyright (C) 2006, 2007 Andrew Ziem
+ * Copyright (C) 2004-2006 Fridrich Strba (fridrich.strba@bluewin.ch)
+ * Copyright (C) 2004 Marc Maurer (uwog@uwog.net)
+ * Copyright (C) 2003-2005 William Lachance (william.lachance@sympatico.ca)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * For further information visit http://libmwaw.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#include "MWAWDebug.hxx"
+#include "MWAWFontConverter.hxx"
+#include "MWAWInputStream.hxx"
+
+#include "MSWStruct.hxx"
+
+namespace MSWStruct
+{
+// ------ font -------------
+std::ostream &operator<<(std::ostream &o, Font const &font)
+{
+  char const *what[Font::NumFlags]= {
+    "b", "it", "strikeout", "outline", "shadow", "smallCaps", "allCaps",
+    "hidden", "underline"
+  };
+  for (int i=0; i < Font::NumFlags; i++) {
+    if (!font.m_flags[i].isSet()) continue;
+    o << what[i];
+    switch(font.m_flags[i].get()) {
+    case 0x80:
+      o << "=no*";
+      break;
+    case 0:
+      o << "=no";
+      break;
+    case 0x81:
+      o << "*";
+    case 1:
+      break;
+    default:
+      o << "=" << std::hex << font.m_flags[i].get() << ",";
+    }
+    o << ",";
+  }
+  if (font.m_unknown.get())
+    o << "ft=" << std::hex << font.m_unknown.get() << std::dec << ",";
+  if (font.m_size.isSet() && font.m_size.get() != font.m_font->size())
+    o << "#size2=" << font.m_size.get() << ",";
+  if (font.m_value.isSet()) o << "id?=" << font.m_value.get() << ",";
+  if (font.m_extra.length())
+    o << font.m_extra << ",";
+  return o;
+}
+
+void Font::insert(Font const &font)
+{
+  uint32_t flags = getFlags();
+  if (flags) m_font->setFlags(flags);
+  m_font.insert(font.m_font);
+  m_size.insert(font.m_size);
+  m_value.insert(font.m_value);
+  m_unknown.insert(font.m_unknown);
+  for (int i = 0; i < NumFlags; i++)
+    m_flags[i] = font.m_flags[i];
+  m_extra+=font.m_extra;
+}
+
+uint32_t Font::getFlags() const
+{
+  uint32_t res=0;
+  uint32_t const fl[NumFlags] = {
+    MWAW_BOLD_BIT, MWAW_ITALICS_BIT, MWAW_STRIKEOUT_BIT, MWAW_OUTLINE_BIT,
+    MWAW_SHADOW_BIT, MWAW_SMALL_CAPS_BIT, MWAW_ALL_CAPS_BIT, MWAW_HIDDEN_BIT,
+    MWAW_UNDERLINE_BIT
+  };
+  if (m_font.isSet()) res = m_font->flags();
+  for (int i=0; i < NumFlags; i++) {
+    if (!m_flags[i].isSet()) continue;
+    int action = m_flags[i].get();
+    if (action&0xFF7E) continue;
+    if (action & 1) res|=fl[i];
+    else res &= ~(fl[i]);
+  }
+  return res;
+}
+
+// ------ section -------------
+bool Section::read(MWAWInputStreamPtr &input, long endPos)
+{
+  long pos = input->tell();
+  long dSz = endPos-pos;
+  if (dSz < 1) return false;
+  libmwaw::DebugStream f;
+  int c = (int) input->readULong(1), val;
+  switch(c) {
+  case 0x75: // column break
+    if (dSz < 2) return false;
+    val = (int) input->readLong(1);
+    switch(val) {
+    case 0:
+      m_colBreak = false;
+      return true;
+    case 1:
+      m_colBreak = true;
+      return true;
+    default:
+      f << "#f75=" << val << ",";
+      break;
+    }
+    break;
+  case 0x77: // num column
+    if (dSz<3) return false;
+    m_col = (int) input->readLong(2)+1;
+    return true;
+  case 0x78:
+    if (dSz<3) return false;
+    m_colSep = float(input->readULong(2))/1440.f;
+    return true;
+    // FIXME: UNKNOWN
+  case 0x80: // a small number: 0, 8, 10
+  case 0x76: // always 1 ?
+  case 0x79:
+  case 0x7d: // always 1
+  case 0x7e: // always 0
+    if (dSz<2) return false;
+    f << "f" << std::hex << c << std::dec << "=" << input->readLong(1) << ",";
+    break;
+  case 0x7b: // 2e1 3d08
+  case 0x7c: // 4da, 6a5, 15ff
+    if (dSz<3) return false;
+    f << "f" << std::hex << c << std::dec << "=";
+    f << std::hex << input->readULong(1) << std::dec << ":";
+    f << std::hex << input->readULong(1) << std::dec << ",";
+    break;
+  case 0x82: // find one time with 168 (related to 7e ?)
+    if (dSz<3) return false;
+    f << "f" << std::hex << c << std::dec << "=" << input->readLong(2) << ",";
+    break;
+  case 0x83:
+  case 0x84:
+    if (dSz < 3) return false;
+    val = (int) input->readLong(2);
+    if (c == 0x83) f << "header[top]=" << val/1440. << ",";
+    else f << "header[bottom]=" << val/1440. << ",";
+    break;
+  default:
+    return false;
+  }
+  m_error += f.str();
+  return true;
+}
+
+std::ostream &operator<<(std::ostream &o, Section const &section)
+{
+  if (section.m_type.get())
+    o << "type=" << std::hex << section.m_type.get() << std::dec << ",";
+  if (section.m_paragraphId.isSet() && section.m_paragraphId.get() > -9999)
+    o << "sP=" << section.m_paragraphId.get() << ",";
+  if (section.m_col.isSet() && section.m_col.get() != 1)
+    o << "cols=" << section.m_col.get() << ",";
+  if (section.m_colSep.isSet())
+    o << "colSep=" << section.m_colSep.get() << "in,";
+  if (section.m_colBreak.get()) o << "colBreak,";
+  if (section.m_flag.get())
+    o << "fl=" << std::hex << section.m_flag.get() << std::dec << ",";
+  if (section.m_error.length()) o << section.m_error << ",";
+  return o;
+}
+
+// paragraph
+std::ostream &operator<<(std::ostream &o, Paragraph::Cell const &cell)
+{
+  if (cell.hasBorders()) {
+    o << "border=";
+    if (cell.m_borders[0]) o << "T";
+    if (cell.m_borders[1]) o << "L";
+    if (cell.m_borders[2]) o << "B";
+    if (cell.m_borders[3]) o << "R";
+    o << ",";
+  }
+  if (cell.m_extra.length()) o << cell.m_extra;
+  return o;
+}
+
+bool Paragraph::read(MWAWInputStreamPtr &input, long endPos)
+{
+  long pos = input->tell();
+  bool sectionSet = m_section.isSet();
+  if (m_section->read(input,endPos))
+    return true;
+  if (!sectionSet) m_section.setSet(false);
+
+  input->seek(pos, WPX_SEEK_SET);
+  long dSz = endPos-pos;
+  if (dSz < 1) return false;
+  libmwaw::DebugStream f, f2;
+  int c = (int) input->readULong(1), val;
+  switch(c) {
+  case 0x5:
+    if (dSz < 2) return false;
+    val = (int) input->readLong(1);
+    switch (val) {
+    case 0:
+      return true;
+    case 1:
+      m_justify = libmwaw::JustificationCenter;
+      return true;
+    case 2:
+      m_justify = libmwaw::JustificationRight;
+      return true;
+    case 3:
+      m_justify = libmwaw::JustificationFull;
+      return true;
+    default:
+      MWAW_DEBUG_MSG(("MSWStruct::Paragraph::read: can not read align\n"));
+      f << "#align=" << val << ",";
+      break;
+    }
+    break;
+  case 0x6: // a small number : always 1 ?
+  case 0x7:
+  case 0x8:
+  case 0x9:
+    if (dSz < 2) return false;
+    val = (int) input->readLong(1);
+    switch(c) {
+    case 7:
+      f << "keeplineTogether";
+      break;
+    case 8:
+      f << "keepwithnext";
+      break;
+    case 9:
+      f << "pagebreakbefore";
+      break;
+    default:
+      f << "f" << std::hex << c << std::dec;
+      break;
+    }
+    if (val==0) f << "*";
+    else if (val != 1) f << "=" << val;
+    f << ",";
+    break;
+  case 0xf: { // tabs
+    int sz = (int) input->readULong(1);
+    if (sz<2 || 2+sz > dSz) {
+      MWAW_DEBUG_MSG(("MSWStruct::Paragraph::read: can not read tab\n"));
+      return false;
+    }
+    int N0 = (int) input->readULong(1);
+    if (2*N0 > sz) {
+      MWAW_DEBUG_MSG(("MSWStruct::Paragraph::read: num tab0 seems odd\n"));
+      return false;
+    }
+    if (N0) { // CHECKME: an increasing list, but what is it ? Often N0=0 or N ?
+      f << "tabs0?=[";
+      for (int i = 0; i < N0; i++) {
+        f << float(input->readLong(2))/1440.f << ",";
+      }
+      f << "],";
+    }
+    int N = (int) input->readULong(1);
+    if (N*3+2*N0+2 != sz ) {
+      MWAW_DEBUG_MSG(("MSWStruct::Paragraph::read: num tab seems odd\n"));
+      f << "#";
+      m_extra += f.str();
+      return false;
+    }
+    std::vector<float> tabs;
+    tabs.resize((size_t) N);
+    for (int i = 0; i < N; i++) tabs[(size_t) i] = float(input->readLong(2))/1440.f;
+    for (int i = 0; i < N; i++) {
+      MWAWTabStop tab;
+      tab.m_position = tabs[(size_t) i];
+      val = (int) input->readULong(1);
+      switch(val>>5) {
+      case 0:
+        break;
+      case 1:
+        tab.m_alignment = MWAWTabStop::CENTER;
+        break;
+      case 2:
+        tab.m_alignment = MWAWTabStop::RIGHT;
+        break;
+      case 3:
+        tab.m_alignment = MWAWTabStop::DECIMAL;
+        break;
+      case 4:
+        tab.m_alignment = MWAWTabStop::BAR;
+        break;
+      default:
+        f << "#tabAlign=" << int(val>>5) << ",";
+        break;
+      }
+      switch((val>>2)&3) {
+      case 1:
+        tab.m_leaderCharacter = '.';
+        break;
+      case 2:
+        tab.m_leaderCharacter = '-';
+        break;
+      case 3:
+        tab.m_leaderCharacter = '_';
+        break;
+      case 0:
+      default:
+        break;
+      }
+      if (val & 0x13)
+        f << "#tabsFlags=" << std::hex << (val & 0x13) << ",";
+      m_tabs->push_back(tab);
+    }
+    break;
+  }
+  case 0x10: // right
+  case 0x11: // left
+  case 0x13: // first left
+    if (dSz < 3) return false;
+    val = (int) input->readLong(2);
+    if (c == 0x13) m_margins[0] = val/1440.;
+    else if (c == 0x11) m_margins[1] = val/1440.;
+    else m_margins[2] = val/1440.;
+    return true;
+  case 0x14: // alignement : 240 normal, 480 : double, ..
+  case 0x15: // spacing before DWIPP
+  case 0x16: // spacing after DWIPP
+    if (dSz < 3) return false;
+    val = (int) input->readLong(2);
+    m_spacings[c-0x14] = val/1440.;
+    if (c != 0x14) return true;
+    m_spacingsInterlineUnit = WPX_INCH;
+    if (val > 0)
+      m_extra += "interline[atLeast],";
+    else if (val < 0)
+      *(m_spacings[0]) *= -1;
+    else {
+      m_spacings[0] = 1.0;
+      m_spacingsInterlineUnit = WPX_PERCENT;
+    }
+    return true;
+  case 0x18:
+  case 0x19:
+    if (dSz < 2) return false;
+    val = (int) input->readULong(1);
+    if (c==0x18) m_inCell = (val!=0);
+    else m_tableDef = (val != 0);
+    if (val != 0 && val!=1)
+      f << "#f" << std::hex << c << std::dec << "=" << val << ",";
+    return true;
+  case 0x1e:
+  case 0x1f:
+  case 0x20:
+  case 0x21:
+  case 0x22:
+    if (dSz < 3) return false;
+    val = (int) input->readULong(2);
+    switch(c) {
+    case 0x1e:
+      *m_border |= libmwaw::TopBorderBit;
+      break;
+    case 0x1f:
+      *m_border |= libmwaw::LeftBorderBit;
+      break;
+    case 0x20:
+      *m_border |= libmwaw::BottomBorderBit;
+      break;
+    case 0x21:
+      *m_border |= libmwaw::RightBorderBit;
+      break;
+    default:
+    case 0x22:
+      f << "border[middle]";
+      break;
+    }
+    if (val & 0x7E00) f << ":textSep=" << int((val& 0x8E00)>>9) << "pt";
+    if (val & 0x8000) f << "*";
+    /* 49: double, 40: normal*/
+    if (val & 0x1FF) f << ":fl=" << std::hex << (val & 0x1FF) << std::dec << ",";
+    break;
+  case 0x98: { // tabs columns
+    int sz = (int) input->readULong(2);
+    if (!sz || dSz < 2+sz) return false;
+    int N = (int) input->readULong(1);
+    if (1+(N+1)*2 > sz) {
+      MWAW_DEBUG_MSG(("MSWStruct::Paragraph::read: table definition seems odd\n"));
+      f << "#tableDef";
+      input->seek(pos+2+sz, WPX_SEEK_SET);
+      break;
+    }
+    for (int i=0; i <= N; i++)
+      m_tableColumns->push_back(float(input->readLong(2)/20.0));
+    int N1 = (sz-(N+1)*2-2);
+    for (int i=0; i < (N1+8)/10; i++) {
+      Cell cell;
+      int numElt= (N1-10*i)/2;
+      if (numElt>5) numElt=5;
+      f2.str("");
+      val = (int) input->readULong(2);
+      switch(val) {
+      case 0:
+        break;
+      case 0x4000:
+        f2 << "empty,";
+        break;
+      case 0x8000:
+        f2 << "merge,";
+        break;
+      default:
+        f2 << "#type=" << std::hex << val << std::dec << ",";
+      }
+      for (int b = 0; b < numElt-1; b++) {
+        val = (int) input->readULong(2);
+        if (!val) continue;
+        cell.m_borders[b] = true;
+        if (val != 0x40)
+          f2 << "#bord" << b << "=" << std::hex << val << std::dec << ",";
+      }
+      cell.m_extra = f2.str();
+      m_tableCells->push_back(cell);
+    }
+    input->seek(pos+2+sz, WPX_SEEK_SET);
+    return true;
+  }
+  default:
+    return false;
+  }
+  m_extra += f.str();
+  return true;
+}
+
+// paragraph
+std::ostream &operator<<(std::ostream &o, Paragraph const &ind)
+{
+  if (ind.m_dim.isSet() && (ind.m_dim.get()[0]>0 || ind.m_dim.get()[1]>0))
+    o << "dim=" << ind.m_dim.get() << ",";
+  o << reinterpret_cast<MWAWParagraph const &>(ind);
+  if (ind.m_section.isSet()) o << ind.m_section.get() << ",";
+  if (ind.m_inCell.get()) o << "cell,";
+  if (ind.m_tableDef.get()) o << "table[def],";
+  if (ind.m_tableColumns->size()) {
+    o << "tableCols=[";
+    for (size_t i = 0; i < ind.m_tableColumns->size(); i++)
+      o << ind.m_tableColumns.get()[i] << ",";
+    o << "],";
+  }
+  if (ind.m_tableCells->size()) {
+    o << "tableCells=[";
+    for (size_t i = 0; i < ind.m_tableCells->size(); i++)
+      o << "[" << ind.m_tableCells.get()[i] << "],";
+    o << "],";
+  }
+  return o;
+}
+
+bool Paragraph::getFont(Font &font) const
+{
+  bool res = true;
+  if (m_font2.isSet())
+    font = m_font2.get();
+  else if (m_font.isSet())
+    font = m_font.get();
+  else
+    res = false;
+  if (m_modFont.isSet()) {
+    font.insert(*m_modFont);
+    res = true;
+  }
+  return res;
+}
+
+void Paragraph::print(std::ostream &o, MWAWFontConverterPtr m_convertissor) const
+{
+  if (m_font2.isSet())
+    o << "font2=[" << m_font2->m_font->getDebugString(m_convertissor) << m_font2.get() << "],";
+  if (m_font.isSet())
+    o << "font=[" << m_font->m_font->getDebugString(m_convertissor) << m_font.get() << "],";
+  if (m_modFont.isSet())
+    o << "modifFont=[" << m_modFont->m_font->getDebugString(m_convertissor) << m_modFont.get() << "],";
+  o << *this;
+}
+}
---
+++ src/lib/MSWStruct.hxx
@@ -0,0 +1,201 @@
+/* -*- Mode: C++; c-default-style: "k&r"; indent-tabs-mode: nil; tab-width: 2; c-basic-offset: 2 -*- */
+/* libmwaw
+ * Copyright (C) 2009, 2011 Alonso Laurent (alonso@loria.fr)
+ * Copyright (C) 2006, 2007 Andrew Ziem
+ * Copyright (C) 2004-2006 Fridrich Strba (fridrich.strba@bluewin.ch)
+ * Copyright (C) 2004 Marc Maurer (uwog@uwog.net)
+ * Copyright (C) 2003-2005 William Lachance (william.lachance@sympatico.ca)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * For further information visit http://libwps.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+/*
+ * Class to read/store the MSW structures
+ */
+
+#ifndef MSW_STRUCT
+#  define MSW_STRUCT
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "libmwaw_internal.hxx"
+
+#include "MWAWFont.hxx"
+#include "MWAWParagraph.hxx"
+
+class MWAWInputStream;
+typedef shared_ptr<MWAWInputStream> MWAWInputStreamPtr;
+class MWAWFontConverter;
+typedef shared_ptr<MWAWFontConverter> MWAWFontConverterPtr;
+
+namespace MSWStruct
+{
+//! the font structure
+struct Font {
+  enum { NumFlags=9 };
+
+  //! the constructor
+  Font(): m_font(MWAWFont(-1,0)), m_size(0), m_value(0), m_unknown(0), m_extra("") {
+    for (int i = 0; i < NumFlags; i++) m_flags[i]=Variable<int>(0);
+  }
+
+  //! insert new font data ( beginning by updating font flags )
+  void insert(Font const &font);
+
+  //! returns the font flags
+  uint32_t getFlags() const;
+
+  //! operator<<
+  friend std::ostream &operator<<(std::ostream &o, Font const &font);
+
+  //! the font
+  Variable<MWAWFont> m_font;
+  //! a second size
+  Variable<int> m_size;
+  //! a unknown value
+  Variable<int> m_value;
+  //! a list of flags
+  Variable<int> m_flags[NumFlags];
+  //! some unknown flag
+  Variable<int> m_unknown;
+  //! extra data
+  std::string m_extra;
+};
+
+//! the section structure
+struct Section {
+  //! constructor
+  Section() : m_id(-1), m_type(0), m_paragraphId(-9999), m_col(1),
+    m_colSep(0.5), m_colBreak(false), m_flag(0), m_error("") {
+  }
+  //! insert the new values
+  void insert(Section const &sec) {
+    m_id.insert(sec.m_id);
+    m_type.insert(sec.m_type);
+    m_paragraphId.insert(sec.m_paragraphId);
+    m_col.insert(sec.m_col);
+    m_colSep.insert(sec.m_colSep);
+    m_colBreak.insert(sec.m_colBreak);
+    m_flag.insert(sec.m_flag);
+    m_error+=sec.m_error;
+  }
+  //! try to read a data
+  bool read(MWAWInputStreamPtr &input, long endPos);
+
+  //! operator<<
+  friend std::ostream &operator<<(std::ostream &o, Section const &section);
+
+  //! the identificator
+  Variable<int> m_id;
+  //! the type
+  Variable<int> m_type;
+  //! the paragraph id
+  Variable<int> m_paragraphId;
+  //! the num of columns
+  Variable<int> m_col;
+  //! the spacing between column
+  Variable<float> m_colSep;
+  //! only a column break
+  Variable<bool> m_colBreak;
+  //! some flag ( in the main position)
+  Variable<int> m_flag;
+  /** the errors */
+  std::string m_error;
+};
+
+//! the paragraph structure
+struct Paragraph : public MWAWParagraph {
+  struct Cell;
+
+  //! Constructor
+  Paragraph() : m_dim(), m_font(), m_font2(), m_modFont(), m_section(),
+    m_inCell(false), m_tableDef(false), m_tableColumns(), m_tableCells() {
+  }
+  //! insert the new values
+  void insert(Paragraph const &para, bool insertModif=true) {
+    m_dim.insert(para.m_dim);
+    if (!m_font.isSet())
+      m_font=para.m_font;
+    else if (para.m_font.isSet())
+      m_font->insert(*para.m_font);
+    if (!m_font2.isSet())
+      m_font2 = para.m_font2;
+    else if (para.m_font2.isSet())
+      m_font2->insert(*para.m_font2);
+    if (insertModif)
+      m_modFont->insert(*para.m_modFont);
+    if (!m_section.isSet())
+      m_section = para.m_section;
+    else if (para.m_section.isSet())
+      m_section->insert(*para.m_section);
+    m_inCell.insert(para.m_inCell);
+    m_tableDef.insert(para.m_tableDef);
+    m_tableColumns.insert(para.m_tableColumns);
+    m_tableCells.insert(para.m_tableCells);
+  }
+  //! try to read a data
+  bool read(MWAWInputStreamPtr &input, long endPos);
+  //! returns the font which correspond to the paragraph if possible
+  bool getFont(Font &font) const;
+  //! operator<<
+  friend std::ostream &operator<<(std::ostream &o, Paragraph const &ind);
+
+  //! operator<<
+  void print(std::ostream &o, MWAWFontConverterPtr m_convertissor) const;
+
+  //! the dimension
+  Variable<Vec2f> m_dim;
+  //! the font (simplified)
+  Variable<Font> m_font, m_font2 /** font ( not simplified )*/, m_modFont /** font (modifier) */;
+  //! the section
+  Variable<Section> m_section;
+  //! a cell/textbox
+  Variable<bool> m_inCell;
+  //! a table
+  Variable<bool> m_tableDef;
+  //! the table columns
+  Variable<std::vector<float> > m_tableColumns;
+  //! the table cells
+  Variable<std::vector<Cell> > m_tableCells;
+
+  //! the cells definitions
+  struct Cell {
+    Cell() : m_extra("") {
+      for (int i = 0; i < 4; i++) m_borders[i] = false;
+    }
+    bool hasBorders() const {
+      for (int i = 0; i < 4; i++)
+        if (m_borders[i]) return true;
+      return false;
+    }
+    //! operator<<
+    friend std::ostream &operator<<(std::ostream &o, Cell const &cell);
+    /* the borders TLBR */
+    bool m_borders[4];
+    /* extra data */
+    std::string m_extra;
+  };
+};
+}
+#endif
+// vim: set filetype=cpp tabstop=2 shiftwidth=2 cindent autoindent smartindent noexpandtab:
