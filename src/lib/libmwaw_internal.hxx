af4a54fab1d8a62bcf2cdc5cc403608ba309140b
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-03-13 16:00:24 +0100

    Ouch, hope it is the last one :)

--- src/lib/libmwaw_internal.hxx
+++ src/lib/libmwaw_internal.hxx
@@ -60,6 +60,7 @@ typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
 typedef unsigned __int64 uint64_t;
+typedef __int64 int64_t;
 
 #else /* !_MSC_VER && !__DJGPP__*/
 
