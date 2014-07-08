421a02c81e7613773bf424a6811517cce47764e4
Caolán McNamara <caolanm@redhat.com>
2014-07-08 15:10:07 +0100

    coverity#1000714 Big parameter passed by value
    and
    
    coverity#1000715 Big parameter passed by value

--- src/lib/libmwaw_internal.hxx
+++ src/lib/libmwaw_internal.hxx
@@ -353,7 +353,7 @@ template <class T> struct Variable {
   //! constructor
   Variable() : m_data(), m_set(false) {}
   //! constructor with a default value
-  Variable(T def) : m_data(def), m_set(false) {}
+  Variable(T const &def) : m_data(def), m_set(false) {}
   //! copy constructor
   Variable(Variable const &orig) : m_data(orig.m_data), m_set(orig.m_set) {}
   //! copy operator
@@ -365,7 +365,8 @@ template <class T> struct Variable {
     return *this;
   }
   //! set a value
-  Variable &operator=(T val) {
+  Variable &operator=(T const &val)
+  {
     m_data = val;
     m_set = true;
     return *this;
